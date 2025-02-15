5710956ed32c02f7f885e66b87b6980f308eb4fb
Ante Ledenko <ledenk96@gmail.com>
2025-02-15 19:17:05 +0000

    Fix database configuration

--- big_brain_idea/settings.py
+++ big_brain_idea/settings.py
@@ -88,7 +88,9 @@ WSGI_APPLICATION = 'big_brain_idea.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+    )
 }
 
 
