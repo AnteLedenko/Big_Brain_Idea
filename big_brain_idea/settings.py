4ef86e120f9100eeebca9730d8a4b6a869c179fb
Ante Ledenko <ledenk96@gmail.com>
2025-02-15 21:15:56 +0000

    Fix4

--- big_brain_idea/settings.py
+++ big_brain_idea/settings.py
@@ -31,8 +31,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-#ALLOWED_HOSTS = ['localhost', '127.0.0.1',os.environ.get('RENDER_HOSTNAME'), 'big-brain-idea.onrender.com']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1',os.environ.get('RENDER_HOSTNAME'), 'big-brain-idea.onrender.com']
+
 # Application definition
 
 INSTALLED_APPS = [
