4aada4319fd1d452ab6ace7d3c577cde020794c6
Ante Ledenko <ledenk96@gmail.com>
2025-02-15 19:48:19 +0000

    Updated database settings to use environment variables

--- big_brain_idea/settings.py
+++ big_brain_idea/settings.py
@@ -88,9 +88,14 @@ WSGI_APPLICATION = 'big_brain_idea.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-    )
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+    }
 }
 
 
